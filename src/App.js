34d29f7d9cde604a4bb15ac2fe304c522827cd8e
Silvia Fioranelli <fioranellisilvia@gmail.com>
2021-07-25 11:03:07 +0100

    Handle API loading and error cases

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import { useState, useEffect } from 'react';
 import List from './components/List';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [{ isLoading, isError, data }, setData] = useState({
+    isLoading: false,
+    isError: false,
+    data: null,
+  });
 
   useEffect(() => {
+    setData({ isLoading: true, isError: false });
     fetch('https://api.thecatapi.com/v1/breeds')
-      .then((res) => res.json())
-      .then((res) => setData(res))
-      .catch((error) => console.log(error));
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Error');
+        }
+      })
+      .then((res) => setData({ data: res, isLoading: false, isError: false }))
+      .catch(() => setData({ isLoading: false, isError: true }));
   }, []);
 
-  if (!data) {
-    return <p>Loading...</p>;
-  }
+  const renderList = () => {
+    if (isError) {
+      return <h5 className='text-center'>Error, please retry!</h5>;
+    }
+  
+    if (isLoading) {
+      return <h5 className="text-center">Loading...</h5>;
+    }
+
+    return <List items={data} />;
+  };
 
   return (
     <div className='container'>
       <h1 className='text-center'>The cat breeds list</h1>
-      <List items={data} />
+      {renderList()}
     </div>
   );
 }
